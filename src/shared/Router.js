27dda6b71b184ac88cb935d228774dce762d085c
Apriyadi Hegarto <ahegarto@gmail.com>
2022-11-14 19:37:51 +0700

    tambahan dari apri

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Test from "../pages/Test";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
